05e0312aa943ddc1875219a4a09b8b11bc16e71e
ANT0NI0737BR <105250786+ANT0NI0737BR@users.noreply.github.com>
2024-02-20 18:24:28 -0300

    Add files via upload

---
+++ media ponderada.py
@@ -0,0 +1,10 @@
+#       (valor, peso)
+valor1 = (100 , 1)
+valor2 = (100 , 1)
+valor3 = (100 , 1)
+valor4 = (100 , 1)
+valor5 = (100 , 1)
+
+print((valor1[0]*valor1[1]+valor2[0]*valor2[1]+valor3[0]*valor3[1]+valor4[0]*valor4[1]+valor5[0]*valor5[1]) / (valor1[1]+valor2[1]+valor3[1]+valor4[1]+valor5[1])      )
+
+#made by https://github.com/ANT0NI0737BR/codigos.git
---
+++ temporizador.py
@@ -0,0 +1,21 @@
+import time as tm
+
+tempo = 2 #duração entre a repetição do codigo
+
+tempoinicio = float(tm.time()) 
+
+numerodevezes = 0
+
+while True:
+
+    tempodesdoinicio = ((tm.time()) - tempoinicio - (tempo * numerodevezes)) #a quanto tempo o programa está rodando
+
+    if tempodesdoinicio > tempo:  #o codigo a ser executado deve ser colocado aqui dentro!
+         
+        print('olá')
+
+        numerodevezes = numerodevezes + 1
+
+        print(tempodesdoinicio)
+
+#made by https://github.com/ANT0NI0737BR/codigos.git
